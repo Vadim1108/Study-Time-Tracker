e968bb3014d4eef19c2e10b63e014e7d3e68d8b2
vadymkononykhin <vadymkononykhin@VADYMKONONY1334>
2022-04-08 19:48:19 +0300

    Create path and work folder

--- Study Time Tracker/Form1.Designer.cs
+++ Study Time Tracker/Form1.Designer.cs
@@ -215,6 +215,7 @@
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "Study Time Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Study Time Tracker/Form1.cs
+++ Study Time Tracker/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,17 @@ namespace Study_Time_Tracker
 {
     public partial class Form1 : Form
     {
+        string path = @"C:\temp\Study Time Tracker"; // Add path
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        // 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(path)) {Directory.CreateDirectory(path);} // Create work folder
 
+        }
     }
 }
